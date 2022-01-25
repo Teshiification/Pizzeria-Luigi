96b2e2059920553a9beed5182001bbfa65d915da
Danny <panini62@web.de>
2022-01-25 10:30:56 +0100

    JavaScript Outsourcing

---
+++ js/ionicons.js
@@ -0,0 +1,10 @@
+/*
+
+ICON IMPORT 
+Imports an specific icon from theyr Website
+Website: https://ionic.io
+
+
+*/
+<script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
+
---
+++ js/particleanimation.js
@@ -0,0 +1,3 @@
+$(function () {
+    $("#animationcontent").load("snowflakes.html");
+  });
