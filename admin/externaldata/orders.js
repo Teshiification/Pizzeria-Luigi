5059cdc481ba5d00ada69fffefae0ec1d65dfa52
Danny <54819795+Teshiification@users.noreply.github.com>
2022-03-18 16:43:34 +0100

    Create orders.js
    Placeholder Orders

---
+++ admin/externaldata/orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        "productName": "Pizza Margarita",
+        "productNumber": 80001,
+        "paymentStatus": "Due",
+        "shipping": "Delivered"
+    },
+    {
+        "productName": "Pizza Spinaci",
+        "productNumber": 80002,
+        "paymentStatus": "Due",
+        "shipping": "Pending"
+    },
+    {
+        "productName": "Pizza Tuna",
+        "productNumber": 80003,
+        "paymentStatus": "Due",
+        "shipping": "Declined"
+    }
+]
